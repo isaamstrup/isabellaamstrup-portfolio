1429934db876753b23133acaa0a686ba11601f75
Isa <isa@Andreass-MacBook-Air.local>
2025-09-17 12:34:36 +0200

    with all changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import './Components/Header.jsx'
 import Header from './Components/Header.jsx'
 
 function App() {
@@ -11,7 +10,7 @@ function App() {
     <>
      <Header/>
      <main>
-      
+
      </main>
     </>
   )
