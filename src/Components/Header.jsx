844971752be0ddfa94ecbf248e89d85e9585f712
Isa <isa@Andreass-MacBook-Air.local>
2025-09-17 12:03:31 +0200

    new design

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header classname="Header">
+      <title>Welcome</title>
+      <nav>
+        <a href="/">Home</a>
+        <a href="/Projects">Projects</a>
+        <a href="/Contact">Contact</a>
+      </nav>
+    </header>
+  )
+}
